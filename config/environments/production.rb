9ae3188a96e87d9d06a494acc227a1cd9e1c8e75
yzhanguvm <yucanzhang@gmail.com>
2014-05-20 16:05:04 -0400

    Modify production environment to allow CSS on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Prototype::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
