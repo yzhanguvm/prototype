21edd2a3e891d48557db7b43c1fe373d257ca0ec
yzhanguvm <yucanzhang@gmail.com>
2014-05-20 11:51:45 -0400

    Add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "PubMedMiner" }
+
   describe "Home page" do
 
     it "should have the content 'PubMedMiner'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("PubMedMiner | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -23,7 +25,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("PubMedMiner | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   
@@ -35,7 +37,21 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("PubMedMiner | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
+
 end
