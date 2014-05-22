35b135340be2cd547a935fe52c9aa6780472e748
yzhanguvm <yucanzhang@gmail.com>
2014-05-22 15:29:26 -0400

    Finish sign-in and sign-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -16,4 +17,17 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
